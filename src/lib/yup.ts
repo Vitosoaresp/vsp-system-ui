4f10c2fcb704fa392f8b794225068d1730a2b659
vitor <pereiravitor1218@gmail.com>
2024-02-24 00:50:22 -0300

    feat: add ui components

---
+++ src/lib/yup.ts
@@ -0,0 +1,21 @@
+import * as y from 'yup';
+
+export const yup = y.setLocale({
+	mixed: {
+		required: 'Campo obrigatório',
+	},
+	string: {
+		email: 'E-mail inválido',
+		length: 'Deve ter exatamente ${length} caracteres',
+		min: 'Deve ter pelo menos ${min} caracteres',
+		max: 'Deve ter no máximo ${max} caracteres',
+	},
+	number: {
+		min: 'Deve ser no mínimo ${min}',
+		max: 'Deve ser no máximo ${max}',
+	},
+	array: {
+		min: 'Deve ter no mínimo ${min} itens',
+		max: 'Deve ter no máximo ${max} itens',
+	},
+});
