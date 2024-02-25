681d463a84f57f50b7fc369311bf764fd081a195
vitor <pereiravitor1218@gmail.com>
2024-02-25 10:14:44 -0300

    feat: update sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -18,15 +18,15 @@ const navItems: NavItemProps[] = [
 
 export const Sidebar = () => {
 	return (
-		<aside className='h-screen border-r border-r-zinc-800 md:w-52 py-5 w-20'>
-			<div className='flex gap-3 items-center mb-10'>
-				<Building2 size={32} className='text-zinc-50' />
-				<div className='h-6 w-px bg-zinc-500' />
-				<h1 className='text-zinc-50 font-semibold'>VSP System</h1>
+		<aside className="h-screen border-r border-r-zinc-800 md:w-52 py-5 w-20">
+			<div className="flex gap-3 items-center mb-10">
+				<Building2 size={32} className="text-zinc-50" />
+				<div className="h-6 w-px bg-zinc-500" />
+				<h1 className="text-zinc-50 font-semibold">VSP System</h1>
 			</div>
 
-			<nav className='flex flex-col pr-4 gap-2'>
-				{navItems.map((item) => (
+			<nav className="flex flex-col pr-4 gap-2">
+				{navItems.map(item => (
 					<NavItem key={item.to} {...item} />
 				))}
 			</nav>
