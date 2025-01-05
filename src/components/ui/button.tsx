703b333a090de2cea24739db5967217a1fdf8cb4
vitor <pereiravitor1218@gmail.com>
2025-01-05 13:49:00 -0300

    feat: add Badge component and enhance Button and Select components with improved styling and structure

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,33 +7,41 @@ import { buttonVariants } from '@/utils/buttons-variants';
 import { Loader2 } from 'lucide-react';
 
 export interface ButtonProps
-	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-		VariantProps<typeof buttonVariants> {
-	asChild?: boolean;
-	loading?: boolean;
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-	(
-		{ className, variant, size, asChild = false, loading, children, ...props },
-		ref,
-	) => {
-		const Comp = asChild ? Slot : 'button';
-		return (
-			<Comp
-				className={cn(
-					'uppercase',
-					buttonVariants({ variant, size, className }),
-				)}
-				ref={ref}
-				disabled={loading || props.disabled}
-				{...props}
-			>
-				{loading && <Loader2 className="text-zinc-900 animate-spin text-xl" />}
-				{!loading && children}
-			</Comp>
-		);
-	},
+  (
+    { className, variant, size, asChild = false, loading, children, ...props },
+    ref,
+  ) => {
+    const Comp = asChild ? Slot : 'button';
+    return (
+      <Comp
+        className={cn('uppercase', buttonVariants({ variant, size, className }))}
+        ref={ref}
+        disabled={loading || props.disabled}
+        {...props}
+      >
+        {loading && (
+          <Loader2
+            className={cn(
+              'text-zinc-900 animate-spin text-xl absolute',
+              loading ? 'visible' : 'invisible',
+            )}
+          />
+        )}
+        <span
+          className={cn(!loading ? 'visible' : 'invisible', 'flex items-center')}
+        >
+          {children}
+        </span>
+      </Comp>
+    );
+  },
 );
 Button.displayName = 'Button';
 
