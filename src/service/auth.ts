aa9ad8aef089e44faef3ffa3f7a5ac872345983b
vitor <pereiravitor1218@gmail.com>
2025-01-10 22:54:40 -0300

    fix(auth): update getMe function to return User object directly style(css): update chart color variables for improved visibility

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -1,15 +1,14 @@
 import { api } from '@/lib/api';
 import { LoginPayload, LoginResponse, RegisterPayload } from '@/types/auth';
 import { User } from '@/types/user';
-import { AxiosResponse } from 'axios';
 
 export const login = async (payload: LoginPayload): Promise<LoginResponse> => {
   const { data } = await api.post<LoginResponse>('/auth/sign-in', payload);
   return data;
 };
 
-export const getMe = async (): Promise<AxiosResponse<User>> => {
-  const data = await api.get<User>('/users/me');
+export const getMe = async (): Promise<User> => {
+  const { data } = await api.get<User>('/users/me');
   return data;
 };
 
