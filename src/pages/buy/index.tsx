31fd181c30c6476ad2b2ae0726845886870e3f3d
vitor <pereiravitor1218@gmail.com>
2025-02-04 11:43:40 -0300

    feat: add breadcrumb navigation for BuyPage

--- src/pages/buy/index.tsx
+++ src/pages/buy/index.tsx
@@ -1,11 +1,18 @@
 import { Navigation } from '@/components/navigation';
+import { BreadcrumbItem, BreadcrumbLink } from '@/components/ui/breadcrumb';
+import { ShoppingBag } from 'lucide-react';
 import { BuyForm } from './components/buy-form';
 
 export default function BuyPage() {
   return (
     <div className="flex flex-col w-full space-y-10">
       <Navigation>
-        <></>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/clientes" className="flex gap-2 items-center">
+            <ShoppingBag className="size-4" />
+            <span>Entrada NF</span>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
       </Navigation>
 
       <BuyForm />
